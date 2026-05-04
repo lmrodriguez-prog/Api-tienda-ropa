572a7e06a7461852aec09c9423d999db8a9d5474
BancoLou <lmrodriguez@gylgroup.com>
2026-05-04 09:13:08 -0300

    Estructura de Repositories completa

---
+++ src/main/java/com/example/crudGyL/repository/TipoProductoRepository.java
@@ -0,0 +1,10 @@
+package com.example.crudGyL.repository;
+
+import com.example.crudGyL.entity.TipoProducto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TipoProductoRepository extends JpaRepository<TipoProducto, Long> {
+
+}
---
+++ src/main/java/com/example/crudGyL/repository/VentaRepository.java
@@ -0,0 +1,9 @@
+package com.example.crudGyL.repository;
+
+import com.example.crudGyL.entity.Venta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VentaRepository extends JpaRepository<Venta, Long> {
+}
